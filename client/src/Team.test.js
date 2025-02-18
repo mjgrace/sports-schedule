cd8e1c077759b22f9461257476b540d913da52f5
Jason Grace <jason@thegracefamily.net>
2025-02-17 22:45:35 -0500

    Client cleanup

--- client/src/Team.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Team from "./Team";
-
-test("renders learn react link", () => {
-  render(<Team />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
