d9290be63e06dce2d45b03b875a6e531245c4ba5
Jason Grace <jason@thegracefamily.net>
2025-02-25 07:34:28 -0500

    Clean up front-end

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import Content from "./Content";
+// import App from "./App";
+// import Content from "./Content";
 import Season from "./Season";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-    <Content />
     <Season />
   </React.StrictMode>
 );
